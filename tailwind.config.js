cf6ef2d184ef59588d400844f80ab852b9a9b847
Quentin Robert <92162680+Quentin1814@users.noreply.github.com>
2023-12-08 00:09:49 +0100

    Prise en charge du dark mode + header (#1)
    Co-authored-by: Luc'HACK <39491773+lucas-luchack@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export default 
+    darkMode: "media",
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
