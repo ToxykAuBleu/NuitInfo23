c998d44d10e294f82b5a7895171fd9cdefabf984
Quentin ROBERT <qrobert001@iutbayonne.univ-pau.fr>
2023-12-08 06:08:21 +0100

    carousel fonctionnel !

--- pages/index.vue
+++ pages/index.vue
@@ -5,16 +5,41 @@
             <p class="italic">Données du Ministère de l'écologie</p>
         </div>
         <!-- carousel -->
-        <div class="flex flex-wrap justify-between mb-2">
-            <a v-for="article in articles"
-                :key="article.id"
-                :href="article.link"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="text-center w-1/6 mx-5 my-2 border-2 p-2 dark:border-green-800 border-green-400 dark:bg-green-800 dark:text-white text-green-800 border-radius-5 rounded px-2 flex items-center justify-center dark:hover:bg-green-700 hover:bg-green-300"
-            >
-                {{ article.title }}
-            </a>
+        <div class="flex justify-between mb-2 carousel">
+            <div class="carousel-inner">
+                <a
+                    v-for="(article, key) in articles"
+                    :key="key"
+                    :href="article.link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="text-center w-1/6 mx-5 my-2 border-2 p-2 dark:border-green-800 border-green-400 dark:bg-green-800 dark:text-white text-green-800 border-radius-5 rounded px-2 flex items-center justify-center dark:hover:bg-green-700 hover:bg-green-300 carousel-item"
+                    :class="key == showedCarousel ? 'active' : ''">
+                    {{ article.title }}
+                </a>
+            </div>
+            <button
+                @click="prevCarousel"
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExample"
+                data-bs-slide="prev">
+                <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+                @click="nextCarousel"
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExample"
+                data-bs-slide="next">
+                <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
 
         <!-- Informations -->
@@ -33,12 +58,104 @@
             const start = 0;
             const end = 10;
             articles.value = await makeRequest(start, end);
+            console.log(articles.value);
         } catch (error) {
             console.error(error);
+        }
+    }
+
+    const showedCarousel = ref(0);
+
+    async function nextCarousel() {
+        if (showedCarousel.value < Object.keys(articles.value).length - 1) {
+            showedCarousel.value++;
+        } else {
+            showedCarousel.value = 0;
+        }
+    }
+
+    async function prevCarousel() {
+        if (showedCarousel.value > 0) {
+            showedCarousel.value--;
+        } else {
+            showedCarousel.value = Object.keys(articles.value).length - 1;
         }
     }
 
     onMounted(fetchArticles);
 </script>
 
-<style scoped></style>
+<style scoped>
+    .carousel {
+        position: relative;
+    }
+
+    .carousel.pointer-event {
+        touch-action: pan-y;
+    }
+
+    .carousel-inner {
+        position: relative;
+        width: 100%;
+        overflow: hidden;
+    }
+    .carousel-inner::after {
+        display: block;
+        clear: both;
+        content: "";
+    }
+
+    .carousel-item {
+        position: relative;
+        display: none;
+        float: left;
+        width: 100%;
+        margin-right: -100%;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        transition: transform 0.6s ease-in-out;
+    }
+
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+
+    .carousel-control-prev,
+    .carousel-control-next {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        z-index: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 15%;
+        padding: 0;
+        color: #fff;
+        text-align: center;
+        background: none;
+        border: 0;
+        opacity: 0.5;
+        transition: opacity 0.15s ease;
+    }
+
+    .carousel-control-prev:hover,
+    .carousel-control-prev:focus,
+    .carousel-control-next:hover,
+    .carousel-control-next:focus {
+        color: #fff;
+        text-decoration: none;
+        outline: 0;
+        opacity: 0.9;
+    }
+
+    .carousel-control-prev {
+        left: 0;
+    }
+
+    .carousel-control-next {
+        right: 0;
+    }
+</style>
