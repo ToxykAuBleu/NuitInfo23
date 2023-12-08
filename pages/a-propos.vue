64607ca3aef62a216092ed7a7178925c5f537446
Mattis Pingard <mattis.pingard@gmail.com>
2023-12-08 06:27:22 +0100

    [+] Ajout du contenu de la page A Propos

--- pages/a-propos.vue
+++ pages/a-propos.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="flex flex-col items-center justify-center min-h-[87.8vh] py-2">
+    <div class="flex flex-col items-center justify-center min-h-[87.8vh] py-6">
         <h1 class="text-4xl mb-4">À propos</h1>
-        <p class="text-lg text-center max-w-md">
-            Bienvenue sur notre site web ! Nous sommes une entreprise dédiée à
-            fournir les meilleures solutions logicielles. Notre équipe d'experts
-            travaille sans relâche pour créer des produits de qualité qui
-            répondent aux besoins de nos clients.
-        </p>
+        <div class="text-lg text-center max-w-md">
+            <p> Ce site à été réalisé dans le cadre de la Nuit de l'Info,
+                Du Jeudi 7 Décembre 2023 à 16h30 au Vendredi 8 Décembre 2023 à 8h.</p>
+            <img src="../assets/image/logo-n2i-2023.png" alt="" srcset="">
+            <p> Ce projet à été réalisé par l'équipe des Infromagiciens composée de :
+                Lucas ESPIET, Lilou DUFAU et Louis MARSAN de première année de BUT Informatique,
+                Mathieu FOISSAC, Mattis PINGARD et Adrien HIRIBARREN TOUYA de deuxième année de BUT Informatique,
+                Ainsi que Quentin ROBERT et Romain GOUAUD de troisième année de BUT Informatique.</p>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "About",
-    };
+export default {
+    name: "About",
+};
 </script>
 
 <style scoped></style>
